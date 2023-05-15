326a894dd8fb30514b769afb3ba12e34467e5e74
HuricanSpeed <mcg03241@gmail.com>
2023-05-15 13:03:48 +0200

    refactor: commit

---
+++ backend/models/post.model.js
@@ -0,0 +1,14 @@
+// const { DataTypes } = require('sequelize')
+// const sequelize = require('../services/sequelize.service')
+
+// const Post = sequelize.define("posts", {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         allowNull: false
+//     },
+//     price: 
+// })
+
+// module.exports = User
