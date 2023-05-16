a9dc370667df9668b8d1d930d43aa23e1b2f19cf
HuricanSpeed <mcg03241@gmail.com>
2023-05-16 09:28:09 +0200

    feat: added 2 routes

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postController = require('../controllers/post.controller')
 
 router.post("/addpost", postController.addpost)
+router.get("/getposts", postController.getposts)
 
 module.exports = router
