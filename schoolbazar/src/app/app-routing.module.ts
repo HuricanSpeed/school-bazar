750bb5b362575dbfa907082d44a933262f8fbbb3
dejw7n <dejw7n@gmail.com>
2023-05-16 10:05:44 +0200

    Update angular version

--- schoolbazar/src/app/app-routing.module.ts
+++ schoolbazar/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./components/home/home.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: "", component: HomeComponent, pathMatch: "full" }];
+const routes: Routes = [];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
