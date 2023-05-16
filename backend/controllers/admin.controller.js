3b471863bd67e1be093bc2a91ae850e3c60df1c8
HuricanSpeed <mcg03241@gmail.com>
2023-05-16 10:54:44 +0200

    feat: getapprovals

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,5 +1,23 @@
 const User = require("../models/user.model")
 
+const getApprovals = (req, res, next) => {
+    try {
+        User.findAll({
+            where: {
+                active: 0
+            }
+        }).then(result => {
+            if(result.length > 0){
+                res.status(200).json({success: true, message: "Approvals found", approvals: result})
+            } else {
+                res.status(404).json({success: false, message: "No approvals found"})
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
 const approve = (req, res, next) => {
     try {
         User.update({
@@ -30,5 +48,6 @@ const removePost = (req, res, next) => {
 
 module.exports = {
     approve,
-    removePost
+    removePost,
+    getApprovals
 }
