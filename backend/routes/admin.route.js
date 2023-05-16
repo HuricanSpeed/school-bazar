db7962e8bcee5ddfe99d01fd6a971eed5338e2ea
HuricanSpeed <mcg03241@gmail.com>
2023-05-16 09:54:35 +0200

    feat: add removePost

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -5,5 +5,6 @@ const adminController = require("../controllers/admin.controller")
 const validatorMiddleware = require("../middlewares/validator.middleware")
 
 router.post("/approve", validatorMiddleware.accountApprove, adminController.approve)
+router.post("/removepost", validatorMiddleware.removePost, adminController.removePost)
 
 module.exports = router
