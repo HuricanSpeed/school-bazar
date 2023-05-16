f6b3f689c762a680ac7b2672e0b2487fc036fbea
HuricanSpeed <mcg03241@gmail.com>
2023-05-16 09:17:09 +0200

    feat: post model

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,14 +1,33 @@
-// const { DataTypes } = require('sequelize')
-// const sequelize = require('../services/sequelize.service')
+const { DataTypes } = require('sequelize')
+const sequelize = require('../services/sequelize.service')
 
-// const Post = sequelize.define("posts", {
-//     id: {
-//         type: DataTypes.INTEGER,
-//         autoIncrement: true,
-//         primaryKey: true,
-//         allowNull: false
-//     },
-//     price: 
-// })
+const Post = sequelize.define("posts", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    state: {
+        type: DataTypes.TINYINT,
+        allowNull: false
+    }, 
+    grade: {
+        type: DataTypes.TINYINT,
+        allowNull: false
+    }
+})
 
-// module.exports = User
+module.exports = Post
